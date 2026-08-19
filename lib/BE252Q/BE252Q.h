0cde2005eda5bc78c743dbc1e8dfafcc0507cca5
Lucas Colem Nunes <lucascolemns@gmail.com>
2026-08-19 20:02:39 -0300

    Enhance BE252Q class with GPS and calibration features
    Updated the BE252Q class to include GPS functionality and calibration methods.

--- lib/BE252Q/BE252Q.h
+++ lib/BE252Q/BE252Q.h
@@ -1,49 +1,70 @@
-//datasheet:
+//datasheet: https://www.beitian.com/en/sys-pd/837.html
+
+//bibliotecas
 #include <Arduino.h>
 #include <Wire.h>
-#include <avr/io.h> //Desenvolver nossa própria comunicação serial à 115200
-    /*
-    É adicionados os seguintes #define a partir dessa biblioteca: 
+#include <TinyGPS++.h>
 
-        UDR0	    Transmit/Receive data buffer
-        UCSR0A	    Status: RXC0, TXC0, UDRE0, frame error, parity error
-        UCSR0B	    Enable: RXEN0, TXEN0, RXCIE0, TXCIE0, UDRIE0
-        UCSR0C	    Frame format: UCSZ (data bits), UPM (parity), USBS (stop bits)
-        UBRR0H      (byte mais significativo)  Baud rate register (12-bit)
-        UBBR0L	    (byte menos significativo) Baud rate register (12-bit)
-    */
-
+//modularização do código 
 #define BE252Q_H
 #ifdef BE252Q_H
 
 //I2C 
-#define BE_252Q_ID 0x0D
-#define BE_252Q_ADRESS 0x0C 
+#define QMC5883_ADDRESS 0x0D
+#define QMC5883_CONTROL_REG 0x09
 
-//UART 115200 para Atmega328p
-#define F_CPU 16000000UL 
-#define BAUD 115200UL
-#define UBRR_VAL ((F_CPU / (8UL * BAUD)) - 1) // U2X0 mode
-    /*
-    formula para calcular a velocidade de comunicação
-    disponível em https://siliconwit.com/education/embedded-programming-atmega328p/uart-serial-communication/
-    */
+//constantes
+const double pi = 3.1415926535;
 
 class BE252Q
 {
 public:
-    
+    void GPS_BE252Q();
+    void BUSSOLA_init();
+    bool BUSSOLA_BE252Q();
+
+    void  calibragem();
+    float direcaoGraus();
+
+    double getLatitude() const { return latitude; };
+    double getLongitude() const { return longitude; };
+    double getVelocidade() const { return velocidade; };
+
+private:
     //I2C
-    uint8_t lerRegistradorI2C(uint8_t endereco);
     void escreverRegistradorI2C(uint8_t endereco, uint8_t valor);
 
-    //USART
-    void UART_BE252Q();
-    void inicilizar_USART();
-    uint8_t lerUSART();
-    void escreverUSART(uint8_t valor);
+    //dados BE252Q
+    double latitude;
+    double longitude;
+    double velocidade;
+
+    uint16_t X_lsb;
+    uint16_t X_msb; 
+    uint16_t Y_lsb;
+    uint16_t Y_msb;
+    uint16_t Z_lsb;
+    uint16_t Z_msb;
+
+    float X;
+    float Z;
+    float Y;
+
+    float offsetX, offsetY, offsetZ;
+    float scaleX = 1, scaleY = 1, scaleZ = 1;
+
+    int16_t minX = INT16_MAX, minY = INT16_MAX, minZ = INT16_MAX;
+    int16_t maxX = INT16_MIN, maxY = INT16_MIN, maxZ = INT16_MIN;   
     
-    private:
+    float raioX;
+    float raioY;
+    float raioZ;
+    float medio;
+
+    float Xcalibrado;
+    float Ycalibrado;
+
+    float rumo;
 };
 
 #endif
