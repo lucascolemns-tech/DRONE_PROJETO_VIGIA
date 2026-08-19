b4b92da80e5a986fc2efea0e293665f83216e6e4
Lucas Colem Nunes <lucascolemns@gmail.com>
2026-08-19 16:33:56 -0300

    Create BDLC class with method and variable sections
    Added BDLC class with public and private sections.

--- lib/BDLC/BDLC.h
+++ lib/BDLC/BDLC.h
@@ -1,1 +1,19 @@
 
+ex class
+
+
+#ifdef BDLC
+#define BDLC
+
+class BDLC 
+{
+public: //coloca funções q vc chama toda hora aq
+  void leralgo()
+
+private: //coloca VARIAVEIS GLOBAIS aqui
+
+};
+
+#endif
+
+//pra chamar a classe você coloca BDLC::letralgo();
