0f7cbf628e101f920dda7605334c3c8a6d25d92d
Lucas Colem Nunes <lucascolemns@gmail.com>
2026-08-19 16:31:40 -0300

    Update BDLC.ino

--- lib/BDLC/BDLC.ino
+++ lib/BDLC/BDLC.ino
@@ -1,1 +1,11 @@
-awdwad
+void ESC_BLDC(int16_t velocidade)
+{ 
+  if (velocidade < 0)
+    velocidade = 0;
+
+  if (velocidade > 1023)
+    velocidade = 1023;
+
+  int16_t vel_correspondente = map(velocidade, 0, 1023, 0, 180);
+  ESC.write(vel_correspondente);
+}
