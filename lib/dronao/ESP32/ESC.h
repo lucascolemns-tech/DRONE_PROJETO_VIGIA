9234c9035ab4c9c5743c1fe220ad528bd61f7023
arthurdev-01 <arthuracademico01@gmail.com>
2026-09-10 21:05:26 -0300

    Enhance ESC class with servo control and pin definitions
    Added servo control for ESC motors and defined pin constants.

--- lib/dronao/ESP32/ESC.h
+++ lib/dronao/ESP32/ESC.h
@@ -1,10 +1,19 @@
 #ifndef ESC_ESP32_H
 #define ESC_ESP32_H
+#define PINO_M1 12
+#define PINO_M2 13
+#define PINO_M3 14
+#define PINO_M4 15
 
 #include <Arduino.h>
-
+#include <Servo.h>
 class ESC_ESP32
 {
+private:
+    Servo ESC_m1;
+    Servo ESC_m2;
+    Servo ESC_m3;
+    Servo ESC_m4;
 public:
   void inicializarUART(long baud = 115200)
   {
@@ -15,6 +24,19 @@ public:
   void enviarUART(uint16_t m1, uint16_t m2, uint16_t m3, uint16_t m4)
   {
     Serial2.printf("%d,%d,%d,%d\n", m1, m2, m3, m4);
+  }
+  void armarESC(){
+    ESC_m1.attach(PINO_M1);
+    ESC_m2.attach(PINO_M2);
+    ESC_m3.attach(PINO_M3);
+    ESC_m4.attach(PINO_M4);
+
+    ESC_m1.writeMicroseconds(1000);  // throttle mínimo
+    ESC_m2.writeMicroseconds(1000);
+    ESC_m3.writeMicroseconds(1000);
+    ESC_m4.writeMicroseconds(1000);
+
+    delay(10000);
   }
 };
 
